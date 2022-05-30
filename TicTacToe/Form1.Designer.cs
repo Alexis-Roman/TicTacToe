e7dbde6ca2adad542267a3edb764a0ca8d45d128
Alexis-Roman <104246596+Alexis-Roman@users.noreply.github.com>
2022-05-30 09:12:03 +0800

    Create interface and added buttons

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpButton1 = new System.Windows.Forms.Button();
+            this.UpButton2 = new System.Windows.Forms.Button();
+            this.CenterButton2 = new System.Windows.Forms.Button();
+            this.UpButton3 = new System.Windows.Forms.Button();
+            this.CenterButton1 = new System.Windows.Forms.Button();
+            this.CenterButton3 = new System.Windows.Forms.Button();
+            this.DownButton1 = new System.Windows.Forms.Button();
+            this.DownButton2 = new System.Windows.Forms.Button();
+            this.DownButton3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UpButton1
+            // 
+            this.UpButton1.BackColor = System.Drawing.Color.White;
+            this.UpButton1.FlatAppearance.BorderSize = 0;
+            this.UpButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpButton1.ForeColor = System.Drawing.Color.Black;
+            this.UpButton1.Location = new System.Drawing.Point(92, 96);
+            this.UpButton1.Name = "UpButton1";
+            this.UpButton1.Size = new System.Drawing.Size(99, 94);
+            this.UpButton1.TabIndex = 0;
+            this.UpButton1.UseVisualStyleBackColor = false;
+            // 
+            // UpButton2
+            // 
+            this.UpButton2.BackColor = System.Drawing.Color.White;
+            this.UpButton2.FlatAppearance.BorderSize = 0;
+            this.UpButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpButton2.ForeColor = System.Drawing.Color.Black;
+            this.UpButton2.Location = new System.Drawing.Point(197, 96);
+            this.UpButton2.Name = "UpButton2";
+            this.UpButton2.Size = new System.Drawing.Size(95, 94);
+            this.UpButton2.TabIndex = 1;
+            this.UpButton2.UseVisualStyleBackColor = false;
+            // 
+            // CenterButton2
+            // 
+            this.CenterButton2.BackColor = System.Drawing.Color.White;
+            this.CenterButton2.FlatAppearance.BorderSize = 0;
+            this.CenterButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CenterButton2.ForeColor = System.Drawing.Color.Black;
+            this.CenterButton2.Location = new System.Drawing.Point(197, 196);
+            this.CenterButton2.Name = "CenterButton2";
+            this.CenterButton2.Size = new System.Drawing.Size(95, 90);
+            this.CenterButton2.TabIndex = 2;
+            this.CenterButton2.UseVisualStyleBackColor = false;
+            // 
+            // UpButton3
+            // 
+            this.UpButton3.BackColor = System.Drawing.Color.White;
+            this.UpButton3.FlatAppearance.BorderSize = 0;
+            this.UpButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpButton3.ForeColor = System.Drawing.Color.Black;
+            this.UpButton3.Location = new System.Drawing.Point(303, 95);
+            this.UpButton3.Name = "UpButton3";
+            this.UpButton3.Size = new System.Drawing.Size(99, 94);
+            this.UpButton3.TabIndex = 3;
+            this.UpButton3.UseVisualStyleBackColor = false;
+            // 
+            // CenterButton1
+            // 
+            this.CenterButton1.BackColor = System.Drawing.Color.White;
+            this.CenterButton1.FlatAppearance.BorderSize = 0;
+            this.CenterButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CenterButton1.ForeColor = System.Drawing.Color.Black;
+            this.CenterButton1.Location = new System.Drawing.Point(89, 196);
+            this.CenterButton1.Name = "CenterButton1";
+            this.CenterButton1.Size = new System.Drawing.Size(99, 90);
+            this.CenterButton1.TabIndex = 4;
+            this.CenterButton1.UseVisualStyleBackColor = false;
+            // 
+            // CenterButton3
+            // 
+            this.CenterButton3.BackColor = System.Drawing.Color.White;
+            this.CenterButton3.FlatAppearance.BorderSize = 0;
+            this.CenterButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CenterButton3.ForeColor = System.Drawing.Color.Black;
+            this.CenterButton3.Location = new System.Drawing.Point(301, 196);
+            this.CenterButton3.Name = "CenterButton3";
+            this.CenterButton3.Size = new System.Drawing.Size(99, 90);
+            this.CenterButton3.TabIndex = 5;
+            this.CenterButton3.UseVisualStyleBackColor = false;
+            // 
+            // DownButton1
+            // 
+            this.DownButton1.BackColor = System.Drawing.Color.White;
+            this.DownButton1.FlatAppearance.BorderSize = 0;
+            this.DownButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DownButton1.ForeColor = System.Drawing.Color.Black;
+            this.DownButton1.Location = new System.Drawing.Point(90, 292);
+            this.DownButton1.Name = "DownButton1";
+            this.DownButton1.Size = new System.Drawing.Size(99, 94);
+            this.DownButton1.TabIndex = 6;
+            this.DownButton1.UseVisualStyleBackColor = false;
+            // 
+            // DownButton2
+            // 
+            this.DownButton2.BackColor = System.Drawing.Color.White;
+            this.DownButton2.FlatAppearance.BorderSize = 0;
+            this.DownButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DownButton2.ForeColor = System.Drawing.Color.Black;
+            this.DownButton2.Location = new System.Drawing.Point(197, 294);
+            this.DownButton2.Name = "DownButton2";
+            this.DownButton2.Size = new System.Drawing.Size(95, 90);
+            this.DownButton2.TabIndex = 7;
+            this.DownButton2.UseVisualStyleBackColor = false;
+            // 
+            // DownButton3
+            // 
+            this.DownButton3.BackColor = System.Drawing.Color.White;
+            this.DownButton3.FlatAppearance.BorderSize = 0;
+            this.DownButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DownButton3.ForeColor = System.Drawing.Color.Black;
+            this.DownButton3.Location = new System.Drawing.Point(301, 293);
+            this.DownButton3.Name = "DownButton3";
+            this.DownButton3.Size = new System.Drawing.Size(99, 94);
+            this.DownButton3.TabIndex = 8;
+            this.DownButton3.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TicTacToe.Properties.Resources.tictactoe;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(488, 430);
+            this.Controls.Add(this.DownButton3);
+            this.Controls.Add(this.DownButton2);
+            this.Controls.Add(this.DownButton1);
+            this.Controls.Add(this.CenterButton3);
+            this.Controls.Add(this.CenterButton1);
+            this.Controls.Add(this.UpButton3);
+            this.Controls.Add(this.CenterButton2);
+            this.Controls.Add(this.UpButton2);
+            this.Controls.Add(this.UpButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button UpButton1;
+        private Button UpButton2;
+        private Button CenterButton2;
+        private Button UpButton3;
+        private Button CenterButton1;
+        private Button CenterButton3;
+        private Button DownButton1;
+        private Button DownButton2;
+        private Button DownButton3;
+    }
+}
