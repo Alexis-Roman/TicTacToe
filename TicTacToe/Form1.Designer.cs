31b3402a8f492eea6ba5c02ae6fdb3ba2a2a7423
Alexis-Roman <104246596+Alexis-Roman@users.noreply.github.com>
2022-05-30 09:45:50 +0800

    First attempt code for UpButton1 & 2

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TicTacToe
 {
-    partial class Form1
+    partial class TicTacToe
     {
         /// <summary>
         ///  Required designer variable.
@@ -37,6 +37,7 @@
             this.DownButton1 = new System.Windows.Forms.Button();
             this.DownButton2 = new System.Windows.Forms.Button();
             this.DownButton3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // UpButton1
@@ -44,30 +45,35 @@
             this.UpButton1.BackColor = System.Drawing.Color.White;
             this.UpButton1.FlatAppearance.BorderSize = 0;
             this.UpButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpButton1.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.UpButton1.ForeColor = System.Drawing.Color.Black;
             this.UpButton1.Location = new System.Drawing.Point(92, 96);
             this.UpButton1.Name = "UpButton1";
             this.UpButton1.Size = new System.Drawing.Size(99, 94);
             this.UpButton1.TabIndex = 0;
             this.UpButton1.UseVisualStyleBackColor = false;
+            this.UpButton1.Click += new System.EventHandler(this.UpButton1_Click);
             // 
             // UpButton2
             // 
             this.UpButton2.BackColor = System.Drawing.Color.White;
             this.UpButton2.FlatAppearance.BorderSize = 0;
             this.UpButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpButton2.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.UpButton2.ForeColor = System.Drawing.Color.Black;
             this.UpButton2.Location = new System.Drawing.Point(197, 96);
             this.UpButton2.Name = "UpButton2";
             this.UpButton2.Size = new System.Drawing.Size(95, 94);
             this.UpButton2.TabIndex = 1;
             this.UpButton2.UseVisualStyleBackColor = false;
+            this.UpButton2.Click += new System.EventHandler(this.UpButton2_Click);
             // 
             // CenterButton2
             // 
             this.CenterButton2.BackColor = System.Drawing.Color.White;
             this.CenterButton2.FlatAppearance.BorderSize = 0;
             this.CenterButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CenterButton2.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.CenterButton2.ForeColor = System.Drawing.Color.Black;
             this.CenterButton2.Location = new System.Drawing.Point(197, 196);
             this.CenterButton2.Name = "CenterButton2";
@@ -80,6 +86,7 @@
             this.UpButton3.BackColor = System.Drawing.Color.White;
             this.UpButton3.FlatAppearance.BorderSize = 0;
             this.UpButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpButton3.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.UpButton3.ForeColor = System.Drawing.Color.Black;
             this.UpButton3.Location = new System.Drawing.Point(303, 95);
             this.UpButton3.Name = "UpButton3";
@@ -92,6 +99,7 @@
             this.CenterButton1.BackColor = System.Drawing.Color.White;
             this.CenterButton1.FlatAppearance.BorderSize = 0;
             this.CenterButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CenterButton1.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.CenterButton1.ForeColor = System.Drawing.Color.Black;
             this.CenterButton1.Location = new System.Drawing.Point(89, 196);
             this.CenterButton1.Name = "CenterButton1";
@@ -104,6 +112,7 @@
             this.CenterButton3.BackColor = System.Drawing.Color.White;
             this.CenterButton3.FlatAppearance.BorderSize = 0;
             this.CenterButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CenterButton3.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.CenterButton3.ForeColor = System.Drawing.Color.Black;
             this.CenterButton3.Location = new System.Drawing.Point(301, 196);
             this.CenterButton3.Name = "CenterButton3";
@@ -116,6 +125,7 @@
             this.DownButton1.BackColor = System.Drawing.Color.White;
             this.DownButton1.FlatAppearance.BorderSize = 0;
             this.DownButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DownButton1.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.DownButton1.ForeColor = System.Drawing.Color.Black;
             this.DownButton1.Location = new System.Drawing.Point(90, 292);
             this.DownButton1.Name = "DownButton1";
@@ -128,6 +138,7 @@
             this.DownButton2.BackColor = System.Drawing.Color.White;
             this.DownButton2.FlatAppearance.BorderSize = 0;
             this.DownButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DownButton2.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.DownButton2.ForeColor = System.Drawing.Color.Black;
             this.DownButton2.Location = new System.Drawing.Point(197, 294);
             this.DownButton2.Name = "DownButton2";
@@ -140,6 +151,7 @@
             this.DownButton3.BackColor = System.Drawing.Color.White;
             this.DownButton3.FlatAppearance.BorderSize = 0;
             this.DownButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DownButton3.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.DownButton3.ForeColor = System.Drawing.Color.Black;
             this.DownButton3.Location = new System.Drawing.Point(301, 293);
             this.DownButton3.Name = "DownButton3";
@@ -147,13 +159,25 @@
             this.DownButton3.TabIndex = 8;
             this.DownButton3.UseVisualStyleBackColor = false;
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("SimSun", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(170, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 30);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "TicTacToe";
+            // 
+            // TicTacToe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::TicTacToe.Properties.Resources.tictactoe;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(488, 430);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.DownButton3);
             this.Controls.Add(this.DownButton2);
             this.Controls.Add(this.DownButton1);
@@ -163,9 +187,12 @@
             this.Controls.Add(this.CenterButton2);
             this.Controls.Add(this.UpButton2);
             this.Controls.Add(this.UpButton1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "TicTacToe";
+            this.Text = "TicTacToe";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -180,5 +207,6 @@
         private Button DownButton1;
         private Button DownButton2;
         private Button DownButton3;
+        private Label label1;
     }
 }
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -1,10 +1,31 @@
 namespace TicTacToe
-{
-    public partial class Form1 : Form
+{            /*
+            1. If button clicked, then print "X" in button text.
+            2. Button became unclickable
+            3. Next button text when clicked print "O", then become unclickable
+            4. Create player 1 win/ player 2 win pop up after the game
+                (need to create player 1 and 2 then add corresponding symbol X/O 
+                (maybe later))
+             */
+    public partial class TicTacToe : Form
     {
-        public Form1()
+        public TicTacToe()
         {
             InitializeComponent();
+        }
+
+        private void UpButton1_Click(object sender, EventArgs e)
+        {
+            UpButton1.Text = ("X");
+            
+        }
+
+        private void UpButton2_Click(object sender, EventArgs e)
+        {
+            if (UpButton1.Text == "X")
+                UpButton2.Text = ("O");
+            else
+                UpButton2.Text = ("X");
         }
     }
 }
